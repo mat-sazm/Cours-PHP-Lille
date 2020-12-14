885f0e3ce92a08e8d84d7051a409acda2d4588c9
Arnaud BODEL <contact@osw3.net>
2020-12-14 14:35:50 +0100

    -

---
+++ 03-constantes/index.php
@@ -0,0 +1,57 @@
+<?php
+$title = "03 - Constantes";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+</head>
+<body>
+    <h1><?= $title ?></h1>
+
+    <p>Un espace mémoire du serveur, qui prend le type et la taille de ce qu'on lui affecte ET qui reste fixe de la déclaration jusqu'a la fin du programme.</p>
+    
+
+    <h2>Déclaration d'une constante</h2>
+
+    <p>On utilise la fonction <code>define()</code> pour déclarer une constante</p>
+    <p>Param 1 : Nom de la constante</p>
+    <p>Param 2 : Valeur de la constante</p>
+    <p>Param 3 : Boolean "case_insensitive" </p>
+
+    <?php
+    define("MA_CONSTANTE", "La valeur de la constante");
+    ?>
+
+    <hr>
+
+    <p>On utilise le mot clé <code>const</code></p>
+
+    <?php
+    const MA_SECONDE_CONSTANTE = 42;
+    ?>
+
+
+
+    <h2>Règles de nommage</h2>
+    <ul>
+        <li>Peut contenir des caracères Alpahbétique, Numérique et _</li>
+        <li>NE PEUT PAS contenir de caractère spécial : <s>&"(èéàç'+-.;?,/\</s></li>
+        <li>NE PEUT PAS commencer par un Numérique</li>
+        <li>Toujours en majuscule.</li>
+    </ul>
+
+
+    <h2>Appel d'une constante</h2>
+
+    <?php var_dump( MA_CONSTANTE ) ?>
+    <br>
+    <?php var_dump( MA_SECONDE_CONSTANTE ) ?>
+
+    <hr>
+
+    <a href="/">Retour</a>
+</body>
+</html>
