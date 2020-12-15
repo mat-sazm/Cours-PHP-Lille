f0d50bb9403b743e84d6ffdcc64e8fd0a5264d1d
Arnaud BODEL <contact@osw3.net>
2020-12-15 10:59:31 +0100

    -

---
+++ 10-tableaux/tp.php
@@ -0,0 +1,48 @@
+<?php 
+// Déclaration des données
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Titre de la page</h1>
+
+    <h2>Instuctions</h2>
+
+    <pre>
+1. Remplacer le "Titre de la page" et le titre du Document par la chaine "TP sur les tableaux" stocké dans la variable $title.
+2. Créer un tableau "heros" et ajouter les héros suivant:
+    
+    Héros 1
+    name: Batman
+    firstname: Bruce
+    lastname: Wayne
+    city: Gotham
+
+    Héros 2
+    name: Superman
+    firstname: Clark
+    lastname: Kent
+    city: New York
+
+    Héros 3
+    name: Ironman
+    firstname: Tony
+    lastname: Stark 
+    city: Los Angeles
+
+3. pour chaque héros afficher la phrase :
+    "XXX XXX dit: Je suis XXX, je protège XXX et ses habitants !"
+    </pre>
+
+    <p>Héros 1 : xxxxx</p>
+    <p>Héros 2 : xxxxx</p>
+    <p>Héros 3 : xxxxx</p>
+    
+</body>
+</html>
