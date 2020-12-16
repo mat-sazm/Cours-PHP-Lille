31b8b798933d491f735cc08b7c7c8ece15293da2
Arnaud BODEL <contact@osw3.net>
2020-12-16 13:04:00 +0100

    -

--- 16-forms/controle.php
+++ 16-forms/controle.php
@@ -17,6 +17,40 @@ error_reporting(E_ALL);
 // Déclaration de la liste des mois
 $month_text = ["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Décembre"];
 
+
+// Controle du formulaire
+if ( $_SERVER['REQUEST_METHOD'] === "POST" )
+{
+    // Recup des données
+    // --
+
+    $firstname = $_POST['firstname'];
+
+
+
+    // Controle des données
+    // --
+
+    // Firstname : Chaine de caractères obligatoire
+    if (strlen($firstname) == 0)
+    {
+        echo "Erreur sur le champ Firstname<br>";
+    }
+
+    // Lastname : Chaine de caractères obligatoire
+
+    // Birthday : doit etre une date valide dans le passé + Min 21 ans
+
+    // Email : Syntaxe email et valeur obligatoire
+
+    // Password : min 4 caractères, 1 alpha + 1 numerique
+
+    // Password Confirmation : doit etre identique à password
+
+    // Agree Terms : Obligatoire
+
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -30,6 +64,18 @@ $month_text = ["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Aout"
     <h1>TP Formulaires</h1>
 
     <pre style="background-color: #C0C0C0; padding: 15px; color: #000000">Method HTTP : <?= $_SERVER['REQUEST_METHOD'] ?>
+
+
+    <?php
+    if ( $_SERVER['REQUEST_METHOD'] === "POST" )
+    {
+        echo "Traite les données et affiche du form";
+    }
+    else
+    {
+        echo "Affiche uniquement le form";
+    }
+    ?>
 
 $_POST :
 <?php print_r($_POST) ?>
