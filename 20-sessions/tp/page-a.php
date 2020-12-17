eeec2dfe4108bf035a0437ffbe126fa45fbed2f2
Arnaud BODEL <contact@osw3.net>
2020-12-17 14:09:47 +0100

    -

---
+++ 20-sessions/tp/page-a.php
@@ -0,0 +1,8 @@
+<?php
+// Creer 3 pages 
+//  -Page A
+//  -Page B
+//  -Page C
+
+// Ajouter un formulaire pour recup le nom de l'utilisateur sur la page A
+// Afficher le nom de l'utilisateur sur les pages A, B et C
